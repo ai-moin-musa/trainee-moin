4550893a6234dd502cb0284578d7a282e2e0ea6f
ai-moin-musa <moin.musa@icreativetechnolabs.com>
2025-01-23 18:55:51 +0530

    [modify] add functions

--- Python_practical_set_1/store.py
+++ Python_practical_set_1/store.py
@@ -13,6 +13,12 @@ class store:
 	def display_all_shelves(self):
 		return [key for key in self.shelves.keys()]
 	
+	def display_products(self):
+		for shelf_name,shelf_obj in mystore.shelves.items():
+			print(f"{shelf_name} --> ",end="")
+			for product_name,product_obj in shelf_obj.products.items():
+				print(f"{product_name}",end=" ")
+			print("\n")
 		
 class shelf:
 	
@@ -32,18 +38,18 @@ class shelf:
 			
 	def get_min_max_sale_price(self):
 		min_max_values_of_product_on_shelf = {}
-		for product in products.values():
-			min_value_product = product.get_min(self)
-			max_value_produdt = product.get_max(self)
+		for product in self.products.values():
+			min_value_product = product.get_min()
+			max_value_product = product.get_max()
 			min_max_values_of_product_on_shelf[product.product_name] = [min_value_product,max_value_product]
 		return min_max_values_of_product_on_shelf
 	
 	def avg_cost_sale_based_on_month(self,month):
 		avg_cost_of_products = []
 		avg_sale_of_products = []
-		for product in products.values():
+		for product in self.products.values():
 			avg_cost_of_products.append(product.get_avg_cost_by_month(month))
-			avg_sale_of_products.append(product.get_sale_cost_by_month(month))
+			avg_sale_of_products.append(product.get_avg_sale_by_month(month))
 			
 		avg_cost_of_shelf = (sum(avg_cost_of_products)/len(avg_cost_of_products))
 		avg_sale_of_shelf = (sum(avg_sale_of_products)/len(avg_sale_of_products))
@@ -53,7 +59,7 @@ class shelf:
 	
 	def profit(self,month):
 		profit = []
-		for product in products.values():
+		for product in self.products.values():
 			profit.append(product.get_profit_by_month(month))
 		return sum(profit)
 			
@@ -66,7 +72,7 @@ class product:
 		self.month_cp_sp = {}
 
 		
-	def set_category(category):
+	def set_category(self,category):
 		self.category = category
 		
 	def set_cost_price(self,month,cp,margin):
@@ -83,8 +89,20 @@ class product:
 
 	#method to update sale price with a given percentage.
 	
+	def update_sale_price_by_product(self,month,new_margin):
+		print(f"old sp :{self.month_cp_sp[month]['sp']}")
+		self.month_cp_sp[month]['sp'] = [(price + (price*new_margin)/100) for price in (self.month_cp_sp[month]['cp'])]
+		print(f"new sp :{self.month_cp_sp[month]['sp']}")
+		return True
+	
 	def update_sale_price(self,new_margin):
-		self.month_cp_sp[month]['sp'] = [(price + (price*margin)/100) for price in (self.month_cp_sp[month]['cp'])]
+		for month in self.month_cp_sp.keys():
+			self.month_cp_sp[month]['sp'] = [(price + (price*new_margin)/100) for price in (self.month_cp_sp[month]['cp'])]
+		
+	
+		
+	def get_months(self):
+		return self.month_cp_sp.keys()
 		
 	def reset_cp(self,month):
 		self.month_cp_sp[month]['cp'] = [0]
@@ -149,11 +167,54 @@ def continueOrNotWithMessage(message):
             print("Invalid input. Please enter 'y' or 'n'.")
 
 
+def update_sale_price():
+	try:
+	
+		shelf = str(input("choose shelf : "))
+		if shelf not in mystore.shelves.keys():
+			print("Invalid input!!! please Try again")
+			return Fasle
+		product = str(input("choose product : "))
+		if product not in (mystore.shelves[shelf].products.keys()):
+			print("Inavalid input!!! please Try again")
+			return False
+		product_obj = mystore.shelves[shelf].products[product]
+		print(list(mystore.shelves[shelf].products[product].get_months()))
+		month = str(input("choose month :"))
+		if month not in (mystore.shelves[shelf].products[product].get_months()):
+			print("Invalid input!!! please Try again")
+			return False 
+		margin = int(input("Enter new margin : "))
+		return product_obj.update_sale_price_by_product(month,margin)
+		
+		
+	except Exception as e:
+		print(f"{e}")
+
+def take_product_object():
+	try:
+		shelf = str(input("choose shelf : "))
+		if shelf not in mystore.shelves.keys():
+			print("Invalid input!!! please Try again")
+			return Fasle
+		product = str(input("choose product : "))
+		if product not in (mystore.shelves[shelf].products.keys()):
+			print("Inavalid input!!! please Try again")
+			return False
+		product_obj = mystore.shelves[shelf].products[product]
+		return product_obj
+	except Exception as e:
+		print(f"{e}")
+
+
+	
+	
+
 #excution starting from here.....
 if __name__ == "__main__" :
 	print("Welcome.................")
 	mystore = store()
-	
+	predefined_months = ('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec')
 	while True:
 		
 		print("""
@@ -216,6 +277,7 @@ if __name__ == "__main__" :
 					continue
 				if sub_choice == 1:
 					while True:
+					
 						product_name = input("enter a product name : ")
 						
 						print(mystore.display_all_shelves())
@@ -231,16 +293,23 @@ if __name__ == "__main__" :
 						print("Added product to the shelf")
 						while True:
 							print("---adding cost price with month----")
-							month = str(input("Enter month: "))
-							cp = list(map(int,input("Enter cost price seperated by space : ").split(" ")))
-							margin = int(input("Enter margin for sale price % :"))
-							product_obj.set_cost_price(month,cp,margin)
-							print("added cost price to the product by month")
-							corn = continueOrNotWithMessage("add another month cost price")
-							if corn:
+							month = str(input("Enter month (ex:'Jan'): "))
+							if month.lower() not in predefined_months:
+								print("Enter valid month!!! Please Try again!!!")
 								continue
-							else:
-								break
+							try:
+								cp = list(map(int,input("Enter cost price seperated by space : ").split(" ")))
+								margin = int(input("Enter margin for sale price % :"))
+								product_obj.set_cost_price(month,cp,margin)
+								print("added cost price to the product by month")
+								corn = continueOrNotWithMessage("add another month cost price")
+								if corn:
+									continue
+								else:
+									break
+							except Exception as e:
+								print("something went wrong!!! please try again!!!")
+								continue
 						
 						corn = continueOrNotWithMessage("add another product")
 						if corn:
@@ -248,11 +317,7 @@ if __name__ == "__main__" :
 						else:
 							break
 				elif sub_choice == 2:
-					for shelf_name,shelf_obj in mystore.shelves.items():
-						print(f"{shelf_name} --> ",end="")
-						for product_name,product_obj in shelf_obj.products.items():
-							print(f"{product_name}",end=" ")
-						print("\n")
+					mystore.display_products()
 						
 				elif sub_choice == 3:
 					break
@@ -262,11 +327,109 @@ if __name__ == "__main__" :
 				
 				
 		elif choice == 3:
-			print("""
-			1. update the sale price with a given percentage.
-			2. update the sale price for a given shelf with a given percentage.
-			
-			""")
+			while True:
+				print("""
+1. update the sale price with a given percentage.
+2. update the sale price for a given shelf with a given percentage.
+3. Method to set a category for a given product.
+4. Method to create a new shelf. ( go through with menu )
+5. Method to reset cost price with 0 for a given shelf, product, and month.
+6. Method to get the maximum or minimum price with the shelf name of a product.
+   Display a proper message that shows the shelf and product.
+7. Define the method and display the Average cost and a sale also profit based on
+   the shelf for a specific month.
+8. Display the Average cost and sales also profit based on the product for a specific
+   month.
+9. exit
+				""")
+				try:
+					sub_choice = int(input("enter choice : "))
+					if sub_choice == 1:
+						mystore.display_products()
+						result = update_sale_price()
+						if result:
+							print("sale price updated successfully.")
+						else:
+							print("sale price is not updated !!! Please Try again.")
+							
+					elif sub_choice == 2:
+						print(mystore.display_all_shelves())
+						shelf = str(input("choose shelf for updating sale price : "))
+						if shelf not in mystore.shelves.keys():
+							print("no shelf found!!! Please try again.")
+							continue
+						shelf_obj = mystore.shelves[shelf]
+						try:
+							margin = int(input("Enter new margin for shelf products : "))
+							shelf_obj.update_sale_price_on_shelf(margin)
+							print("sale price updated on shelf successfully")
+						except Exception as e:
+							print("something went wrong!!! please try again.")
+					elif sub_choice == 3:
+						mystore.display_products()
+						product_obj = take_product_object()
+						if product_obj == False:
+							print("something went wrong please try again")
+							
+						category = str(input("Enter category for the product : "))
+						product_obj.set_category(category)
+						print(f"successfully set category {product_obj.category} for {product_obj.product_name}")
+					elif sub_choice == 4:
+						pass
+					elif sub_choice == 5:
+						mystore.display_products()
+						product_obj = take_product_object()
+						print(list(product_obj.get_months()))
+						month = str(input("choose month :"))
+						if month not in list(product_obj.get_months()):
+							print("Invalid input!!! please Try again")
+							continue
+						product_obj.reset_cp(month)
+						print("successfully reset cost price")
+						 
+					elif sub_choice == 6:
+						for sname,sobject in mystore.shelves.items():
+							result = sobject.get_min_max_sale_price()
+							for key,value in result.items():
+								print(f"{sname} --> {key} --> min: {value[0]} max: {value[1]}")
+					elif sub_choice == 7:
+						print(mystore.display_all_shelves())
+						shelf = str(input("choose shelf: "))
+						if shelf not in mystore.shelves.keys():
+							print("no shelf found!!! Please try again.")
+							continue
+						shelf_obj = mystore.shelves[shelf]
+						month = str(input("Enter month (ex 'Jan'):"))
+						if month not in predefined_months:
+							print("enter valid month")
+							continue
+						avg_cost_price, avg_sale_price = shelf_obj.avg_cost_sale_based_on_month(month)
+						profit_shelf =  shelf_obj.profit(month)
+						print(f"{shelf_obj.shelf_name} Average cost price on {month} is {avg_cost_price}")
+						print(f"{shelf_obj.shelf_name} Average sale price on {month} is {avg_sale_price}")
+						print(f"{shelf_obj.shelf_name} Average profit on {month} is {profit_shelf}")
+					elif sub_choice == 8:
+						mystore.display_products()
+						product_obj = take_product_object()
+						month = str(input("choose month :"))
+						if month not in predefined_months:
+							print("Invalid input!!! please Try again")
+							continue
+						avg_cost = product_obj.get_avg_cost_by_month(month)
+						avg_sale = product_obj.get_avg_sale_by_month(month)
+						profit = product_obj.get_profit_by_month(month)
+						print(f"{product_obj.product_name} Average cost price on {month} is {avg_cost}")
+						print(f"{product_obj.product_name} Average sale price on {month} is {avg_sale}")
+						print(f"{product_obj.product_name} profit on {month} is {profit}")
+					elif sub_choice == 9:
+						break
+					else:
+						print("Invalid choice!!! Please Try again!!!")
+						continue
+				except Exception as e:
+					print(e)
+					#print("something went wrong!!! please try again!!!")
+					continue
 		elif choice == 4:
 			break
 		elif choice == 5:
@@ -281,14 +444,3 @@ if __name__ == "__main__" :
 			print("Invalid choice!!! Please Try again")
 			continue
 			
-			
-		
-	
-	
-
-
-	
-	
-
-
-
